1b27487b4330f907d7c8c3e6f72d776a32e7b1f9
gabrielgageiro <gabrielgageiro@gmail.com>
2024-08-03 21:45:34 -0300

    implementacao do TodoService e TodoController

--- src/main/java/com/example/todolist/todolist/todolist/TodoController.java
+++ src/main/java/com/example/todolist/todolist/todolist/TodoController.java
@@ -1,7 +1,7 @@
+package com.example.todolist.todolist.todolist;
+
 import java.util.List;
 
-import com.example.todolist.todolist.todolist.Todo;
-import com.example.todolist.todolist.todolist.TodoService;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
